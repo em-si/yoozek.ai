27ab391d07ba5bd04a40a3fe17543f7647bbf0eb
PKraska02 <pkraska02@gmail.com>
2025-01-25 17:23:02 +0100

    Commit1.1

--- src/main/assistant/AssistantService.ts
+++ src/main/assistant/AssistantService.ts
@@ -136,6 +136,7 @@ export class AssistantService {
                         content: userPrompt
                     }
                 ],
+                true
             );
 
             // Parsowanie odpowiedzi
